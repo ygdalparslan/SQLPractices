460c8c867434d33378241039ed1961392b2d571b
ygdalparslan <ygdalparslan@gmail.com>
2023-10-16 00:57:22 +0300

    Q5 ile çalışma

--- Q5Practice.sql
+++ Q5Practice.sql
@@ -67,30 +67,62 @@ SELECT name, grade FROM students
 
 
 
-/*------------------DAY'1 HOMEWORK---------------------------------------------------------------------------------------------------
+------------------DAY'1 HOMEWORK---------------------------------------------------------------------------------------------------
 				   GIRIS- Database Olusturma;
 				   
-ÖDEVV:)
-1) Tablo Oluşturma:
+--ÖDEVV:)
+--1) Tablo Oluşturma:
 
-"tedarikciler" isminde bir tablo olusturun,
-"tedarikci_id", "tedarikci_ismi", "tedarikci_adres"
-ve "ulasim_tarihi"	field'lari olsun. 
+-- "tedarikciler" isminde bir tablo olusturun,
+-- "tedarikci_id", "tedarikci_ismi", "tedarikci_adres"
+-- ve "ulasim_tarihi"	field'lari olsun. 
 
-2) Var olan tablodan yeni tablo olusturma:
- "tedarikci_info" isminde bir tabloyu  "tedarikciler" tablosundan olusturun.
-Icinde "tedarikci_ismi", "ulasim_tarihi" field'lari olsun
+create table tedarikciler(
+	tedarikci_id int,
+	tedarikci_ismi varchar(30),
+	tedarikci_adres varchar(50),
+	ulasim_tarihi date
+);
 
-3) Data ekleme:
-" ogretmenler" isminde tablo olusturun.
- Icinde "kimlik_no", "isim", "brans" ve "cinsiyet" field'lari olsun.
-"ogretmenler" tablosuna bilgileri asagidaki gibi olan kisileri ekleyin.
-kimlik_no: 234431223, isim: Ayse Guler, brans : Matematik, cinsiyet: kadin.
-kimlik_no: 234431224, isim: Ali Guler, brans : Fizik, cinsiyet: erkek.
+--2) Var olan tablodan yeni tablo olusturma:
+--  "tedarikci_info" isminde bir tabloyu  "tedarikciler" tablosundan olusturun.
+-- Icinde "tedarikci_ismi", "ulasim_tarihi" field'lari olsun
+create table tedarikci_info as select tedarikci_ismi, ulasim_tarihi from tedarikciler;
+select * from tedarikci_info;
 
-4) bazı fieldları olan kayıt ekleme:
-"ogretmenler" tablosuna bilgileri asagidaki gibi olan bir kisi ekleyin.
-kimlik_no: 567597624, isim: Veli Guler */
+--3) Data ekleme:
+--" ogretmenler" isminde tablo olusturun.
+--  Icinde "kimlik_no", "isim", "brans" ve "cinsiyet" field'lari olsun.
+-- "ogretmenler" tablosuna bilgileri asagidaki gibi olan kisileri ekleyin.
+-- kimlik_no: 234431223, isim: Ayse Guler, brans : Matematik, cinsiyet: kadin.
+-- kimlik_no: 234431224, isim: Ali Guler, brans : Fizik, cinsiyet: erkek.
+create table ogretmenler(
+	kimlik_no char(11) primary key,
+	isim varchar(20) not null,
+	brans varchar(30),
+	cinsiyet char(5)
+);
+
+insert into ogretmenler values ('234431223','Ayse Guler','Matematik','kadin');
+insert into ogretmenler values ('234431224','Ali Guler','Fizik','erkek');
+select * from ogretmenler;
+
+-- 4) bazı fieldları olan kayıt ekleme:
+-- "ogretmenler" tablosuna bilgileri asagidaki gibi olan bir kisi ekleyin.
+-- kimlik_no: 567597624, isim: Veli Guler
+insert into ogretmenler (kimlik_no, isim) values ('567597624','Veli Guler');
+insert into ogretmenler values ('567597625','Ahmet Guler');
+
+
+
+
+
+
+
+
+
+
+
 
 
 
